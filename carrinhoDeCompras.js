2ae324cb898ee90cf619e680cdcfb95a1c3730f2
HenriqueMAlves <m.henriquealves@gmail.com>
2022-10-26 21:26:59 -0300

    feat: adicionar a classe de carrinho de compras

---
+++ carrinhoDeCompras.js
@@ -0,0 +1,33 @@
+class CarrinhoDeCompras {
+    constructor(itens){
+        this.itens = itens;
+    }
+
+    getItens(){
+        return this.itens;
+    }
+
+    getPrecoTotal(){
+        var preco = 0;
+
+        this.itens.forEach(item => {
+            preco += item.preco;
+        });
+
+        return preco;
+    }
+}
+
+var item1 = {
+    nome: "item 1",
+    preco: 40
+};
+var item2 = {
+    nome: "item 2",
+    preco: 90
+};
+const carrinho = new CarrinhoDeCompras([item1, item2]);
+
+console.log("************* Carrinho de compras ************************");
+console.log(carrinho.getItens());
+console.log("Preço total: " + carrinho.getPrecoTotal());
